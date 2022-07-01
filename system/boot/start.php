aa967529fb198b7c10458d346bf3efe17a3ffea7
Gabriel Scatolin <gscatolin@outlook.com>
2022-06-30 23:57:37 -0300

    fix autoload

--- system/boot/start.php
+++ system/boot/start.php
@@ -14,7 +14,18 @@ foreach (glob(INPHINIT_PATH . "util/*.php") as $file) {
     require_once $file;
 }
 
-require_once INPHINIT_PATH . 'lib/__driver.php';
+require_once INPHINIT_PATH . 'lib/constant/datatypes.php';
+require_once INPHINIT_PATH . 'lib/query/where_executor.php';
+require_once INPHINIT_PATH . 'lib/query/query.php';
+require_once INPHINIT_PATH . 'lib/schema/builder.php';
+require_once INPHINIT_PATH . 'lib/schema/validator.php';
+require_once INPHINIT_PATH . 'lib/schema/schema.php';
+require_once INPHINIT_PATH . 'lib/id.php';
+require_once INPHINIT_PATH . 'lib/io.php';
+require_once INPHINIT_PATH . 'lib/name.php';
+require_once INPHINIT_PATH . 'lib/permission.php';
+require_once INPHINIT_PATH . 'lib/rm.php';
+
 require_once INPHINIT_PATH . 'routing.php';
 
 App::exec();
--- system/lib/__driver.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-foreach(rsearch(INPHINIT_PATH . "lib", ["php"]) as $file) {
-    if(basename($file) == "__driver.php") {
-        continue;
-    }
-    require_once $file;
-}
